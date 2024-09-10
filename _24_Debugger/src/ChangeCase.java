192275ccbe055a44eb2e018adfc1591f2a5fc474
Eugene Berezin <esberezin@gmail.com>
2024-09-10 13:35:45 +0500

    Домашнее задание 24 по теме Debugger

---
+++ _24_Debugger/src/ChangeCase.java
@@ -0,0 +1,15 @@
+public class ChangeCase {
+    public static String ChangeCase(String str) {
+        StringBuilder result = new StringBuilder(str.length());
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else if (Character.isLowerCase(c)) {
+                result.append(Character.toUpperCase(c));
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+}
--- _24_Debugger/src/Main.java
+++ _24_Debugger/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 1.    Дан массив целых чисел. Найти максимальное число из четных в этом массиве.
  * 2.    Дана строка "Привет, Александр, с праздником, с Новым Годом". Написать программу,
@@ -6,6 +8,19 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int maxEven = Arrays.stream(numbers)
+                .filter(num -> num % 2 == 0)
+                .max()
+                .orElseThrow(() -> new RuntimeException("Нет четных чисел в массиве"));
+
+        System.out.println("Максимальное четное число: " + maxEven);
+
+        System.out.println("Задание 2");
+        String input = "Привет, Александр, с праздником, с Новым Годом";
+        String output = ChangeCase.ChangeCase(input);
+        System.out.println(output);
+
     }
 }
