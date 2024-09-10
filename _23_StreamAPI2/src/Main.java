8d34ec8162c941f9ba612a5dfb29c7a87f9f2190
Eugene Berezin <esberezin@gmail.com>
2024-09-10 13:26:28 +0500

    Домашнее задание 23 по теме StreamAPI2

--- _23_StreamAPI2/src/Main.java
+++ _23_StreamAPI2/src/Main.java
@@ -1,10 +1,35 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * 1.    Напишите программу для вычисления суммы всех четных и нечетных чисел в списке с использованием потоков.
+ * 1. Напишите программу для вычисления суммы всех четных и нечетных чисел в списке с использованием потоков.
  *
  * 2. Напишите программу для удаления всех повторяющихся элементов из списка с помощью потоков
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        int sumOfEvens = numbers.stream()
+                .filter(num -> num % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        int sumOfOdds = numbers.stream()
+                .filter(num -> num % 2 != 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println("Сумма всех четных чисел: " + sumOfEvens);
+        System.out.println("Сумма всех нечетных чисел: " + sumOfOdds);
+
+        System.out.println("Задание 2");
+        List<String> items = Arrays.asList("1", "2", "3", "1", "2", "4");
+        List<String> uniqueItems = items.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+                System.out.println("Список без повторяющихся элементов: " + uniqueItems);
     }
 }
